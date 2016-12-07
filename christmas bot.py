c1e1f176b5af74313f7da1f6f580d339762e46f1
tankoks <koksengtan98@gmail.com>
2016-12-06 21:59:29 -0500

    Discord Bot using discord.py
    uses discord.py library
    Edit the role name and token and run.

---
+++ christmas bot.py
@@ -0,0 +1,32 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+@asyncio.coroutine
+def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+@asyncio.coroutine
+def on_message(message):
+     rolelist = message.author.roles
+     for x in rolelist:
+        if str(x) == "#insert name of role here eg Mods":
+            if x.colour.value == 3066993:
+                x.colour = x.colour.red()
+                yield from client.edit_role(x.server, x)
+                break
+            if x.colour.value == 15158332:
+                x.colour = x.colour.green()
+                yield from client.edit_role(x.server, x)
+                break
+
+
+
+            
+client.run('#Insert Token Here#')
